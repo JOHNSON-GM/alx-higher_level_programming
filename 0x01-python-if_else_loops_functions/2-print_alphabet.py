360c3f58f9f9603bf610cc059a57fb715419b46f
JOHNSON-GM <gichukijohnson93@gmail.com>
2022-08-24 10:10:41 +0300

    done

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
-for alphabets in range(ord('a'), ord('z')+1):
-    print("{:c}".format(alphabets), end="")
+result = ""
+for c in range(0,26):
+    result += chr(ord('a')+c)
+print (result)
