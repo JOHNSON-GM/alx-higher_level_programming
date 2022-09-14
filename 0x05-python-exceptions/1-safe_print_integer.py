dd6521195e2f1260a23708d025c2bf3a01e0673e
JOHNSON-GM <gichukijohnson93@gmail.com>
2022-09-14 22:51:49 +0300

    task 1 done

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """ Print integers
+    Args:
+        value: what to print
+    Returns:
+        return true or false
+    """
+    if value:
+        try:
+            print("{:d}".format(value))
+            return True
+        except Exception:
+            return False
